b5a88c8dd901b1cc82fd18ca9c4366c33d9707e9
deva0510 <155135755+deva0510@users.noreply.github.com>
2024-04-29 17:37:52 +0530

    Update settings.py

--- Teerdhaproject/settings.py
+++ Teerdhaproject/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 CSRF_TRUSTED_ORIGINS=['https://c8e1-139-167-50-98.ngrok-free.app/']
 
-ALLOWED_HOSTS = ['c8e1-139-167-50-98.ngrok-free.app','localhost','127.0.0.1']
+ALLOWED_HOSTS = ['*','c8e1-139-167-50-98.ngrok-free.app','localhost','127.0.0.1']
 
 
 # Application definition
@@ -83,7 +83,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME':'booking_new',
-        'POST':'3306',
+        'PORT':'3306',
         'PASSWORD':'',
         'HOST':'localhost',
         'USER':'root'
@@ -126,6 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT=os.path.join(BASE_DIR,"static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
